8a53058a48763f982ba2373a2c528db26cea24b6
PinsariDulya <91836960+PinsariDulya@users.noreply.github.com>
2023-09-29 10:32:53 +0530

    Update SumOfIntegers.java
    fix issue in the code

--- SumOfIntegers.java
+++ SumOfIntegers.java
@@ -1,7 +1,7 @@
 public class SumOfIntegers {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; b <= 1; i++) {
+        for (int i = 1; i <= 10; i++) { // Changed 'b' to 'i' and fixed the condition
             sum += i;
         }
         System.out.println("Sum of integers from 1 to 10: " + sum);
