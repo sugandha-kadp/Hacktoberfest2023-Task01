a0d3c438f59a27107b7d8f0f7591bab80f8d5f94
Kavindu Chethani <46352484+kavindu-chethani@users.noreply.github.com>
2023-10-01 15:35:24 +0530

    fixed

--- SumOfIntegers.java
+++ SumOfIntegers.java
@@ -1,9 +1,9 @@
 public class SumOfIntegers {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; b <= 1; i++) {
+        for (int i = 1; i <= 10; i++) {
             sum += i;
         }
         System.out.println("Sum of integers from 1 to 10: " + sum);
     }
-}
+}  
